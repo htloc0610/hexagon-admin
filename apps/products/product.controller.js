bcee7d65e0b3109978f50e49e2770b15418cda45
notDuyLam <22120181@student.hcmus.edu.vn>
2024-12-11 22:01:30 +0700

    update product controller

--- apps/products/product.controller.js
+++ apps/products/product.controller.js
@@ -1,10 +1,23 @@
 const productService = require('./product.service');
 
 const productController = {
-    async getProducts(req, res) {
-        const products = await productService.getProducts();
-        res.json(products);
-    }
+    async getAllProducts() {
+        try {
+            const products = await productService.getAllProducts();
+            return products;
+        } catch (error) {
+            throw new Error('Error retrieving products: ' + error.message);
+        }
+    },
+
+    async getAllCategories() {
+        try {
+            const categories = await productService.getAllCategories();
+            return categories;
+        } catch (error) {
+            throw new Error('Error retrieving categories: ' + error.message);
+        }
+    },
 };
 
 module.exports = productController;
